795356f25d7041e591f4bd546d6f633b00128c0d
Asen Grigorov <canucklehead@abv.bg>
2026-02-23 14:14:30 +0200

    fix/bug-model

--- bugs/models.py
+++ bugs/models.py
@@ -5,10 +5,10 @@ from django.db import models
 
 class Bug(models.Model):
     class BugTypeChoices(models.TextChoices):
-        FIRE = 'Fire', 'Fire'
-        WATER = 'Water', 'Water'
-        EARTH = 'Earth', 'Earth'
-        GRASS = 'Grass', 'Grass'
+        FIRE = 'FIRE', 'Fire'
+        WATER = 'WATER', 'Water'
+        EARTH = 'EARTH', 'Earth'
+        GRASS = 'GRASS', 'Grass'
 
 
     name = models.CharField(
@@ -21,57 +21,65 @@ class Bug(models.Model):
         choices=BugTypeChoices,
     )
 
-    max_health_points = models.DecimalField(
-        max_digits=5,
-        decimal_places=2,
+    max_health_points = models.PositiveIntegerField(
         validators=[
             MinValueValidator(0),
         ],
         verbose_name='Health Points',
     )
 
-    armor = models.DecimalField(
-        max_digits=5,
-        decimal_places=2,
+    armor = models.PositiveIntegerField(
         validators=[
             MinValueValidator(0),
         ],
     )
 
-    strength = models.DecimalField(
-        max_digits=5,
-        decimal_places=2,
+    strength = models.PositiveIntegerField(
         validators=[
             MinValueValidator(0),
         ],
     )
 
-    mobility = models.DecimalField(
-        max_digits=5,
-        decimal_places=2,
+    mobility = models.PositiveIntegerField(
         validators=[
             MinValueValidator(0),
         ],
     )
 
-    healing_factor = models.DecimalField(
-        max_digits=5,
-        decimal_places=2,
+    healing_factor = models.PositiveIntegerField(
         validators=[
             MinValueValidator(0),
         ],
     )
 
-    # natural_habitat = models.ForeignKey(
-    #     to="locations.Location",
-    #     on_delete=models.CASCADE,
-    #     related_name='bugs',
-    # )
+    natural_habitat = models.ForeignKey(
+        to="locations.Location",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='bugs',
+    )
 
     image_url = models.URLField(
         verbose_name='Bug Image',
     )
 
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+
     is_active = models.BooleanField(
         default=False,
     )
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if self.is_active:
+            Bug.objects.filter(is_active=True).update(is_active=False)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ('name',)
