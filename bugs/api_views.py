69498bf244273c554adfc76e76a1c4d24e4bde4d
Asen Grigorov <canucklehead@abv.bg>
2026-04-06 12:31:37 +0300

    permission-fixes

--- bugs/api_views.py
+++ bugs/api_views.py
@@ -2,12 +2,24 @@ from rest_framework import generics, permissions
 from bugs.models import Bug
 from bugs.serializers import BugSerializer
 
+class StrictDjangoModelPermissions(permissions.DjangoModelPermissions):
+    def __init__(self):
+        self.perms_map = {
+            'GET': ['%(app_label)s.view_%(model_name)s'],
+            'OPTIONS': [],
+            'HEAD': ['%(app_label)s.view_%(model_name)s'],
+            'POST': ['%(app_label)s.add_%(model_name)s'],
+            'PUT': ['%(app_label)s.change_%(model_name)s'],
+            'PATCH': ['%(app_label)s.change_%(model_name)s'],
+            'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+        }
+
 class BugListCreateAPIView(generics.ListCreateAPIView):
     queryset = Bug.objects.all()
     serializer_class = BugSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+    permission_classes = [permissions.IsAuthenticated, StrictDjangoModelPermissions]
 
 class BugRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Bug.objects.all()
     serializer_class = BugSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+    permission_classes = [permissions.IsAuthenticated, StrictDjangoModelPermissions]
