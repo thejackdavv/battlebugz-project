132e8d7ce820c2f30552aae3b212cbe726b4df2d
Asen Grigorov <canucklehead@abv.bg>
2026-04-07 15:04:30 +0300

    importmissing

--- bugs/views.py
+++ bugs/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
